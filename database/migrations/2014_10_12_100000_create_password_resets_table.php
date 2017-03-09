a184b5f5c72c5e1849b9fab5f037d9e800e963ae
Ahn <lalala13@sir.kr>
2017-03-09 18:13:12 +0900

    커밋메시지

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,8 +14,8 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
+            $table->string('email',50)->index();
+            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
