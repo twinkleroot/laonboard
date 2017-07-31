af5a0f9771278538eb71aaedabaac1fdb79a751d
Olivia <olivia@sir.kr>
2017-07-31 17:52:55 +0900

    내용관리 리스트, 폼

--- resources/views/admin/contents/form.blade.php
+++ resources/views/admin/contents/form.blade.php
@@ -13,8 +13,25 @@
 @endsection
 
 @section('content')
+<form name="contentform" onsubmit="return contentFormCheck(this);" action="{{ $type == "update" ? route('admin.contents.update', $content->id) : route('admin.contents.store') }}" method="POST" enctype="multipart/form-data">
+<div class="body-head">
+    <div class="pull-left">
+        <h3>내용관리추가</h3>
+        <ul class="fl">
+            <li class="admin">Admin</li>
+            <li class="depth">게시판관리</li>
+            <li class="depth">내용관리추가</li>
+        </ul>
+    </div>
+</div>
+<div id="body_tab_type2">
+    <span class="txt">내용관리를 추가합니다.</span>
+    <div class="submit_btn">
+        <button type="submit" class="btn btn-sir">확인</button>
+        <a class="btn btn-default" href="{{ route('admin.contents.index') }}">목록</a>
+    </div>
+</div>
 <div class="body-contents">
-    <form name="contentform" onsubmit="return contentFormCheck(this);" action="{{ $type == "update" ? route('admin.contents.update', $content->id) : route('admin.contents.store') }}" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="type" value="{{ $type }}">
     <input type="hidden" name="html" value="1">
     @if($type == "update")
@@ -23,138 +40,154 @@
         {{ method_field('POST')}}
     @endif
     {{ csrf_field() }}
-    <table>
-    <caption>내용 관리 목록</caption>
-    <colgroup>
-        <col class="grid_4">
-        <col>
-    </colgroup>
-    <tbody>
-    <tr>
-        <th scope="row"><label for="co_id">ID</label></th>
-        <td>
-            <span class="frm_info">20자 이내의 영문자, 숫자, _ 만 가능합니다.</span><br />
-            <input type="text" name="content_id" id ="content_id" required @if($type == "update") value="{{ $content->content_id }}" readonly @else value="{{ old('content_id') }}" @endif size="20" maxlength="20">
-            @foreach ($errors->get('content_id') as $message)
-                <span class="help-block">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @endforeach
-            @if($type == "update")
-                <a href="{{ route('contents.show', $content->content_id) }}" class="btn_frmline">내용확인</a>
-            @endif
-        </td>
-    </tr>
-    <tr>
-        <th scope="row"><label for="subject">제목</label></th>
-        <td>
-            <input type="text" name="subject" id="subject" @if($type == "update") value="{{ $content->subject }}" @else value="{{ old('subject') }}" @endif required size="90">
-            @foreach ($errors->get('subject') as $message)
-                <span class="help-block">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @endforeach
-        </td>
-    </tr>
-    <tr>
-        <th scope="row">내용</th>
-        <td>
-            <div style="border: 1px solid #ccc; background: #fff; min-height: 400px; border-radius: 4px; box-sizing: border-box;">
-                <textarea name="content" id="content" class="editorArea">@if($type == "update"){{ $content->content }} @else {{ old('content') }} @endif</textarea>
-            </div>
-            @foreach ($errors->get('content') as $message)
-                <span class="help-block">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @endforeach
-        </td>
-    </tr>
-    {{-- <tr>
-        <th scope="row">모바일 내용</th>
-        <td>
-            <div style="border: 1px solid #ccc; background: #fff; min-height: 400px; border-radius: 4px; box-sizing: border-box;">
+    <div class="adm_box_hd">
+        <span class="adm_box_title">내용관리 추가</span>
+    </div>
+    <table class="adm_box_table">
+        <tr>
+            <th>
+                <label for="co_id">ID</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="text" name="content_id" id ="content_id" required @if($type == "update") class="form-control form_input" value="{{ $content->content_id }}" readonly @else class="form-control td_id" value="{{ old('content_id') }}" @endif size="20" maxlength="20">
+                @foreach ($errors->get('content_id') as $message)
+                    <span class="help-block">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @endforeach
+                @if($type == "update")
+                    <a href="{{ route('contents.show', $content->content_id) }}" class="btn btn-sir">내용확인</a>
+                @endif
+                <span class="help-block">20자 이내의 영문자, 숫자, _ 만 가능합니다.</span>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="subject">제목</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="text" name="subject" id="subject" class="form-control form_w90" @if($type == "update") value="{{ $content->subject }}" @else value="{{ old('subject') }}" @endif required size="90">
+                @foreach ($errors->get('subject') as $message)
+                    <span class="help-block">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @endforeach
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="content">내용</label>
+            </th>
+            <td class="table_body chknone">
+                <div style="background: #fff; min-height: 400px; border-radius: 4px; box-sizing: border-box;">
+                    <textarea name="content" id="content" class="editorArea">@if($type == "update"){{ $content->content }} @else {{ old('content') }} @endif</textarea>
+                </div>
+                @foreach ($errors->get('content') as $message)
+                    <span class="help-block">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @endforeach
+            </td>
+        </tr>
+        {{-- <tr>
+            <th>
+                <label for="mobile_content">모바일 내용</label>
+            </th>
+            <td class="table_body chknone">
+                <div style="background: #fff; min-height: 400px; border-radius: 4px; box-sizing: border-box;">
                 <textarea name="mobile_content" id="mobile_content" class="editorArea">@if($type == 'update'){{ $content->mobile_content }}@endif</textarea>
             </div>
-        </td>
-    </tr> --}}
-    <tr>
-        <th scope="row"><label for="skin">스킨 디렉토리</label></th>
-        <td>
-            <select name="skin" id="skin">
-            @foreach($skinList as $key => $value)
-                <option value="{{ $key }}" @if($type == "update" && $key == $content->skin) selected @endif>
-                    {{ $value }}
-                </option>
-            @endforeach
-            </select>
-        </td>
-    </tr>
-    {{-- <tr>
-        <th scope="row"><label for="mobile_skin">모바일스킨 디렉토리</label></th>
-        <td>
-            <select name="mobile_skin" id="mobile_skin">
-            @foreach($mobileSkinList as $key => $value)
-                <option value="{{ $key }}" @if($type == "update" && $key == $content->mobile_skin) selected @endif>
-                    {{ $value }}
-                </option>
-            @endforeach
-            </select>
-        </td>
-    </tr> --}}
-    <tr>
-        <th scope="row"><label for="tag_filter_use">태그 필터링 사용</label></th>
-        <td>
-            <span class="frm_info">내용에서 iframe 등의 태그를 사용하려면 사용안함으로 선택해 주십시오.</span><br />
-            <select name="tag_filter_use" id="tag_filter_use">
-                <option value="0" @if($type == 'update' && $content->tag_filter_use == 0) selected @endif>사용안함</option>
-                <option value="1" @if($type == 'update' && $content->tag_filter_use == 1) selected @endif>사용함</option>
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <th scope="row"><label for="include_head">상단 파일 경로</label></th>
-        <td>
-            <span class="frm_info">설정값이 없으면 기본 상단 파일을 사용합니다.</span><br />
-            <input type="text" name="include_head" @if($type == 'update') value="{{ $content->include_head}}" @endif id="include_head" class="frm_input" size="60">
-        </td>
-    </tr>
-    <tr>
-        <th scope="row"><label for="include_tail">하단 파일 경로</label></th>
-        <td>
-            <span class="frm_info">설정값이 없으면 기본 하단 파일을 사용합니다.</span><br />
-            <input type="text" name="include_tail" @if($type == 'update') value="{{ $content->include_tail}}" @endif id="include_tail" class="frm_input" size="60">
-        </td>
-    </tr>
-    <tr>
-        <th scope="row"><label for="himg">상단이미지</label></th>
-        <td>
-            <input type="file" name="himg" id="himg">
-            @if($type == 'update' && $existHeadImage)
-                <input type="checkbox" name="himg_del" value="1" id="himg_del"> <label for="himg_del">삭제</label>
-                <div class="banner_or_img">
-                    <img src="/storage/content/{{ $content->content_id }}_h" width="{{ $headImageWidth }}" alt="">
-                </div>
-            @endif
-        </td>
-    </tr>
-    <tr>
-        <th scope="row"><label for="timg">하단이미지</label></th>
-        <td>
-            <input type="file" name="timg" id="timg">
-            @if($type == 'update' && $existTailImage)
-                <input type="checkbox" name="timg_del" value="1" id="timg_del"> <label for="timg_del">삭제</label>
-                <div class="banner_or_img">
-                    <img src="/storage/content/{{ $content->content_id }}_t" width="{{ $tailImageWidth }}" alt="">
-                </div>
-            @endif
-        </td>
-    </tr>
-    </tbody>
+            </td>
+        </tr> --}}
+        <tr>
+            <th>
+                <label for="skin">스킨 디렉토리</label>
+            </th>
+            <td class="table_body chknone">
+                <select name="skin" id="skin" class="form-control form_large">
+                @foreach($skinList as $key => $value)
+                    <option value="{{ $key }}" @if($type == "update" && $key == $content->skin) selected @endif>
+                        {{ $value }}
+                    </option>
+                @endforeach
+                </select>
+            </td>
+        </tr>
+        {{-- <tr>
+            <th>
+                <label for="mobile_skin">모바일스킨 디렉토리</label>
+            </th>
+            <td class="table_body chknone">
+                <select name="mobile_skin" id="mobile_skin" class="form-control form_large">
+                @foreach($mobileSkinList as $key => $value)
+                    <option value="{{ $key }}" @if($type == "update" && $key == $content->mobile_skin) selected @endif>
+                        {{ $value }}
+                    </option>
+                @endforeach
+                </select>
+            </td>
+        </tr> --}}
+        <tr>
+            <th>
+                <label for="tag_filter_use">태그 필터링 사용</label>
+            </th>
+            <td class="table_body chknone">
+                <select name="tag_filter_use" id="tag_filter_use" class="form-control form_middle">
+                    <option value="0" @if($type == 'update' && $content->tag_filter_use == 0) selected @endif>사용안함</option>
+                    <option value="1" @if($type == 'update' && $content->tag_filter_use == 1) selected @endif>사용함</option>
+                </select>
+                <span class="help-block">내용에서 iframe 등의 태그를 사용하려면 사용안함으로 선택해 주십시오.</span>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="include_head">상단 파일 경로</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="text" name="include_head" class="form-control form_large" @if($type == 'update') value="{{ $content->include_head}}" @endif id="include_head" class="frm_input" size="60">
+                <span class="help-block">설정값이 없으면 기본 상단 파일을 사용합니다.</span>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="include_tail">하단 파일 경로</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="text" name="include_tail" class="form-control form_large" @if($type == 'update') value="{{ $content->include_tail}}" @endif id="include_tail" class="frm_input" size="60">
+                <span class="help-block">설정값이 없으면 기본 하단 파일을 사용합니다.</span>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="himg">상단이미지</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="file" name="himg" id="himg">
+                @if($type == 'update' && $existHeadImage)
+                    <input type="checkbox" name="himg_del" value="1" id="himg_del"> <label for="himg_del">삭제</label>
+                    <div class="banner_or_img">
+                        <img src="/storage/content/{{ $content->content_id }}_h" width="{{ $headImageWidth }}" alt="">
+                    </div>
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="timg">하단이미지</label>
+            </th>
+            <td class="table_body chknone">
+                <input type="file" name="himg" id="himg">
+                @if($type == 'update' && $existHeadImage)
+                    <input type="checkbox" name="himg_del" value="1" id="himg_del"> <label for="himg_del">삭제</label>
+                    <div class="banner_or_img">
+                        <img src="/storage/content/{{ $content->content_id }}_h" width="{{ $headImageWidth }}" alt="">
+                    </div>
+                @endif
+            </td>
+        </tr>
     </table>
-        <input type="submit" value="확인" class="btn_submit">
-        <a href="{{ route('admin.contents.index') }}">목록</a>
-    </form>
 </div>
+</form>
 <script>
 function contentFormCheck(f)
 {
