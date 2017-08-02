eb64fba04207fb1139eead6566919f2e8d8ad44b
Olivia <olivia@sir.kr>
2017-08-02 17:57:43 +0900

    게시물 복사/이동 디자인 적용

--- resources/views/board/move.blade.php
+++ resources/views/board/move.blade.php
@@ -10,6 +10,11 @@
 
     <title>게시물 {{ $type=='move' ? '이동' : '복사' }} | {{ Cache::get("config.homepage")->title }}</title>
 
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('themes/default/css/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('themes/default/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('font-awesome/css/font-awesome.css') }}">
+
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -21,44 +26,54 @@
     <script src="{{ asset('js/common.js') }}"></script>
 
 </head>
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">게시물 {{ $type=='move' ? '이동' : '복사' }}</div>
-            <form class="form-horizontal" role="form" method="POST" action="" onsubmit="return formMoveListSubmit(this);">
-                <input type="hidden" name="type" value="{{ $type }}" />
-                {{ csrf_field() }}
-                <div class="panel-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th> <!-- 전체선택 -->
-                                    <input type="checkbox" name="chkAll" onclick="checkAll(this.form)">
-                                </th>
-                                <th>게시판</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($boards as $board)
-                            <tr>
-                                <td class="bd_check"><input type="checkbox" name="chkId[]" class="boardId" value='{{ $board->id }}'></td>
-                                <td>
-                                    {{ $board->group->subject . ' > ' . $board->subject . ' (' . $board->table_name . ')' }}
-                                    @if($board->id == $currentBoard->id) 현재 @endif
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <div class="panel-heading">
-                    <input type="submit" class="btn btn-primary" value="{{ $type=='move' ? '이동' : '복사' }}" onclick="document.pressed=this.value"/>
-                    <input type="button" class="btn btn-primary" onclick="window.close();" value="창닫기"/>
-                </div>
-            </form>
+<body>
+<form role="form" method="POST" action="" onsubmit="return formMoveListSubmit(this);">
+    <input type="hidden" name="type" value="{{ $type }}" />
+    {{ csrf_field() }}
+    <div id="header">
+    <div class="container">
+        <div class="title">
+            <span>게시물 {{ $type=='move' ? '이동' : '복사' }}</span>
+        </div>
+        <div class="cbtn">
+            <input type="submit" class="btn btn-sir" value="{{ $type=='move' ? '이동' : '복사' }}" onclick="document.pressed=this.value"/>
+            <input type="button" class="btn btn-default" onclick="window.close();" value="창닫기"/>
         </div>
     </div>
-</div>
+    </div>
+
+    <div id="bd_scrap" class="container">
+        <div class="form-horizontal">
+            <table class="table box">
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" name="chkAll" onclick="checkAll(this.form)"></th>
+                        <th>게시판</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($boards as $board)
+                    <tr @if($board->id == $currentBoard->id) class="on" @endif>
+                        <td class="bd_check">
+                            <input type="checkbox" name="chkId[]" class="boardId" value='{{ $board->id }}'>
+                        </td>
+                        <td>
+                            <label for="chkId[]">
+                                <span class="gr1">{{ $board->group->subject }}</span> <!-- 그룹의 첫번째는 gr1 -->
+                                <i class="fa fa-angle-right"></i>
+                                <span>{{ $board->subject }} ({{ $board->table_name }})</span>
+                            </label>
+                            @if($board->id == $currentBoard->id)
+                                <span class="here">현재</span>
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</form>
 <script>
 // 복사 및 이동 폼 서브밋 전 실행되는 함수
 function formMoveListSubmit(f) {
@@ -74,3 +89,4 @@ function formMoveListSubmit(f) {
     return true;
 }
 </script>
+</body>
