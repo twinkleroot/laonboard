86ce4e79127c071d823eb7a444608437f6584363
Twinkleroot <lalala13@nate.com>
2017-08-09 15:39:41 +0900

    마이그레이션 수정(manage_auth) 3

--- database/migrations/2017_06_29_110439_create_auth_table.php
+++ database/migrations/2017_06_29_110439_create_auth_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class CreateAuthTable extends Migration
 {
