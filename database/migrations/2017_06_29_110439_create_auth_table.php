08fcf9fd94f44a93589204b7fce2047a51205853
Twinkleroot <lalala13@nate.com>
2017-08-09 15:38:22 +0900

    마이그레이션 수정(manage_auth) 2

--- database/migrations/2017_06_29_110439_create_auth_table.php
+++ database/migrations/2017_06_29_110439_create_auth_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use DB;
 
 class CreateAuthTable extends Migration
 {
@@ -22,7 +23,7 @@ class CreateAuthTable extends Migration
         });
 
         // 라라벨 기본 API에서 mysql의 set type을 지원하지 않으므로 enum으로 생성하고 set으로 변경한다.
-        DB::statement("ALTER TABLE ". env('DB_PREFIX'). "manage_auth CHANGE `auth` `auth` SET('r', 'w', 'd');");
+        DB::statement("ALTER TABLE ". DB::getTablePrefix(). "manage_auth CHANGE `auth` `auth` SET('r', 'w', 'd');");
     }
 
     /**
