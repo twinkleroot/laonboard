8d55182554e9ce0a4961d335ff068471744ade12
Twinkleroot <lalala13@nate.com>
2017-08-11 14:49:50 +0900

    설정 변경

--- app/Http/Middleware/CheckInstall.php
+++ app/Http/Middleware/CheckInstall.php
@@ -22,7 +22,12 @@ class CheckInstall
         $path = base_path($file);
         try {
             if(File::exists($path) && env('DB_DATABASE') && env('DB_USERNAME') && env('DB_PASSWORD') && env('DB_PREFIX')) {
+                // DB 연결 확인
                 DB::getPdo();
+                // 기본 설정 테이블 존재 확인
+                if(!Schema::hasTable('configs')) {
+                    return view('install.index');
+                }
                 return $next($request);
             } else {
                 return view('install.index');
