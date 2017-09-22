77a7fb2243ece7cffa8c833db2ceca1b8cba04a5
Twinkleroot <lalala13@nate.com>
2017-09-22 09:47:11 +0900

    css/js 버전업

--- config/gnu.php
+++ config/gnu.php
@@ -3,7 +3,7 @@
 
     // 상수
     'IP_DISPLAY' => '\\1.♡.\\3.\\4',
-    'VER' => Carbon\Carbon::now()->format('Ymd').'-6',
+    'VER' => Carbon\Carbon::now()->format('Ymd').'-1',
 
     // 홈페이지 기본 환경 설정
     'title' => '라라벨게시판',
