a4eaee36887609aa8effdfcc084c59b6f400e8c9
Twinkleroot <lalala13@nate.com>
2017-09-27 15:14:10 +0900

    tinyMCE 에디터에 한글 폰트 적용

--- resources/views/admin/boards/form.blade.php
+++ resources/views/admin/boards/form.blade.php
@@ -1220,16 +1220,22 @@ tinymce.init({
     height: 400,
     min_height: 400,
     min_width: 400,
-    selection_toolbar: 'bold italic | quicklink h2 h3 blockquote',
-    plugins: 'link,autolink,image,imagetools,textcolor,lists,pagebreak,table,save,insertdatetime,preview,media,searchreplace,print,contextmenu,directionality,fullscreen,noneditable,visualchars,',
+    plugins: [
+        'advlist autolink lists link charmap print preview anchor textcolor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime table contextmenu paste code help'
+    ],
     menubar: false,
-    toolbar: "undo redo | fontselect | fontsizeselect | forecolor backcolor bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table link media customImage",
+    toolbar1: "insert | formatselect fontselect fontsizeselect | forecolor backcolor bold italic underline removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
+    toolbar2: "undo redo | customImage table code | preview fullscreen print | help",
     fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
+    font_formats : "굴림=굴림;굴림체=굴림체;궁서=궁서;궁서체=궁서체;돋움=돋움;돋움체=돋움체;바탕=바탕;바탕체=바탕체;나눔고딕=나눔고딕;맑은고딕='맑은 고딕';"
+    +"Arial=Arial;Comic Sans MS='Comic Sans MS';Courier New='Courier New';Tahoma=Tahoma;Times New Roman='Times New Roman';Verdana=Verdana",
     relative_urls: false,
     setup: function(editor) {
         editor.on('init', function() {
             this.getDoc().body.style.fontSize = '10pt';
-            this.getDoc().body.style.fontFamily = 'Arial';
+            this.getDoc().body.style.fontFamily = '굴림체';
         });
         editor.addButton('customImage', {
             text: '사진',
--- resources/views/board/default/form.blade.php
+++ resources/views/board/default/form.blade.php
@@ -288,7 +288,7 @@ tinymce.init({
     mobile: {
         theme: 'beta-mobile',
         plugins: ['autosave', 'lists'],
-        toolbar: [ 'undo', 'redo', 'fontsizeselect', 'forecolor', 'bold', 'italic', 'underline', 'bullist', 'numlist', 'removeformat' ],
+        toolbar: [ 'undo', 'redo', 'fontsizeselect', 'forecolor', 'bold', 'italic', 'underline', 'bullist', 'numlist', 'removeformat' ]
     },
     skin: "lightgray",
     height: 400,
@@ -303,11 +303,13 @@ tinymce.init({
     toolbar1: "insert | formatselect fontselect fontsizeselect | forecolor backcolor bold italic underline removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
     toolbar2: "undo redo | customImage table code | preview fullscreen print | help",
     fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
+    font_formats : "굴림=굴림;굴림체=굴림체;궁서=궁서;궁서체=궁서체;돋움=돋움;돋움체=돋움체;바탕=바탕;바탕체=바탕체;나눔고딕=나눔고딕;맑은고딕='맑은 고딕';"
+    +"Arial=Arial;Comic Sans MS='Comic Sans MS';Courier New='Courier New';Tahoma=Tahoma;Times New Roman='Times New Roman';Verdana=Verdana",
     relative_urls: false,
     setup: function(editor) {
         editor.on('init', function() {
             this.getDoc().body.style.fontSize = '10pt';
-            this.getDoc().body.style.fontFamily = 'Arial';
+            this.getDoc().body.style.fontFamily = '굴림체';
         });
         editor.addButton('customImage', {
             text: '사진',
