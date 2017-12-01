b1148c7ee4bf5365115e3027ce93264cd571770d
Twinkleroot <lalala13@nate.com>
2017-12-01 11:02:15 +0900

    PopularSearch 모듈에서 검색 후 인기검색어 추가하는 로직에서 키워드 종류가 subject, content만 추가하도록 변경

--- Modules/PopularSearches/Listeners/AddPopularKeywordListener.php
+++ Modules/PopularSearches/Listeners/AddPopularKeywordListener.php
@@ -33,7 +33,7 @@ class AddPopularKeywordListener
         $keywords = explode(' ', strip_tags($keyword));                             // 검색어를 구분자로 나눈다.
 
         // 이메일, 글쓴이 검색이 아닐 때만 인기 검색어 추가
-        if(strpos($kind, 'email') === false && strpos($kind, 'name') === false ) {
+        if(str_contains($kind, 'subject') || str_contains($kind, 'content')) {
             foreach($keywords as $word) {
                 $this->addPopular($word);
             }
